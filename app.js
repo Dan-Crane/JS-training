88ad29496a6787f923445da2d6f918dd65866678
dani <gre4kaDISCOUNT@gmail.com>
2020-08-29 16:07:30 +0900

    concat

--- app.js
+++ app.js
@@ -12,13 +12,28 @@ const myLodash = {
 			}
 			return acc
 		}, [])
-	}
+	},
+	concat: (array, ...spred) => {
+		return array.concat(...spred)
+}
 }
 
 const a = [0, 1, 2, 3, 4, '', false, 'oeua', 2, null, undefined]
 const a2 = [0, 1, 2, 3, 4, 2,]
 const b = ['test-0', 'test-1', 'test-2', 'test-3', 'test-4']
 
-console.log(myLodash.chunk(b, 2))
+console.log(myLodash.concat(a2, 4, 5, 7, [['o']], [2]));
+// console.log(myLodash.chunk(b, 2))
 
-console.log(myLodash.compact(a))
+// console.log(myLodash.compact(a))
+
+
+
+// var array = [1];
+// var other = _.concat(array, 2, [3], [[4, '3']]);
+//
+// console.log(other);
+// // => [1, 2, 3, [4]]
+//
+// console.log(array);
+// // => [1]
