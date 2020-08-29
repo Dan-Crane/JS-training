4937a5efea6dddd70cadbf22cec8418ea8a939cb
dani <gre4kaDISCOUNT@gmail.com>
2020-08-29 12:58:15 +0900

    'concat'

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const _ = {
+const myLodash = {
 	chunk: (array, number) => {
 		return [
 			array.slice(0, number),
 			array.slice(number)
 		]
+	},
+	compact: (array) => {
+		return array.reduce((acc, value, index) => {
+			if (value) {
+				return acc.concat(value)
+			}
+			return acc
+		}, [])
 	}
 }
 
-const a = [0, 1, 2, 3, 4]
+const a = [0, 1, 2, 3, 4, '', false, 'oeua', 2, null, undefined]
+const a2 = [0, 1, 2, 3, 4, 2,]
 const b = ['test-0', 'test-1', 'test-2', 'test-3', 'test-4']
 
-console.log(_.chunk(b, 2))
+console.log(myLodash.chunk(b, 2))
+
+console.log(myLodash.compact(a))
